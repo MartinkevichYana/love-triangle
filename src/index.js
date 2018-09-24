42424ceffa2725319737db011750196bcd02bf52
Martinkevich Yana <Yana22685@yandex.ru>
2018-09-24 22:05:16 +0300

    second2

--- src/index.js
+++ src/index.js
@@ -8,13 +8,9 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   var arrChecked = [];
   for(var i = 1; i <= preferences.length; i++) {
   	if(isFitValue(i, arrChecked) == true) {
-  		//alert(i + "    " + arrChecked);
-  		//alert(preferences[preferences[preferences[i-1]-1]-1] + "     " + i+"    "+ preferences[i-1]+"      "+ preferences[preferences[i-1]-1]);
-  		if((preferences[preferences[preferences[i-1]-1]-1] == i) && (preferences[preferences[i-1]-1] != i) && (preferences[i-1] != i+1)) {
+  		if(preferences[preferences[preferences[i-1]-1]-1] == i && preferences[preferences[i-1]-1] != i && preferences[i-1] != i+1) {
   			count++;
-
   			arrChecked.push(i, preferences[i-1], preferences[preferences[i-1]-1]);
-  			//alert(arrChecked);
   		}
   	}
   }
@@ -25,9 +21,8 @@ function isFitValue(ind, arr) {
 	if(arr.length == 0)
 		return true;
 	for(var i = 0; i < arr.length; i++) {
-		if(arr[i] == ind) {
+		if(arr[i] == ind)
 			return false;
-		}
 	}
 	return true;
 }
